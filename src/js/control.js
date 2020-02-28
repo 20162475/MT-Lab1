4f812540e695ac5ce180c7cdf498648247aab841
Ugnius <ugniues@yahoo.com>
2020-02-28 23:19:14 +0200

    Patobulintas stop mygtuko veikimas

--- src/js/control.js
+++ src/js/control.js
@@ -2,9 +2,12 @@ var player = videojs('my-player');
 
 document.getElementById("button-play").addEventListener("click", function() { player.play(); });
 document.getElementById("button-pause").addEventListener("click", function() { player.pause(); });
-document.getElementById("button-stop").addEventListener("click", function() { player.reset(); });
 document.getElementById("button-jump5f").addEventListener("click", function() { player.currentTime(player.currentTime() + 5); });
 document.getElementById("button-jump5b").addEventListener("click", function() { player.currentTime(player.currentTime() - 5); });
+document.getElementById("button-stop").addEventListener("click", function() { 
+    player.pause();
+    player.currentTime(0); 
+});
 
 document.getElementById("button-url1").addEventListener("click", function() { 
     const source = document.getElementsByName('url1')[0].value
